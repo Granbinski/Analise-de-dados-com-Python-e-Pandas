607277c06084b2f31aa76d66c2dc8ce1b665a371
Inacio Grandini Ulbinski <inaciogranbinski@gmail.com>
2022-10-12 01:28:21 -0300

    update

--- Estrutura_De_dados.py
+++ Estrutura_De_dados.py
@@ -1,3 +1,4 @@
+# LISTAS
 # Criando uma lista
 animais = ["cavalo", "galo", 1, 2, 3]
 
@@ -22,7 +23,7 @@ print(len(animais))
 # Verificando se existe na lista
 print("gato" in animais)
 
-numericos = [500, 30, 300, 80, 10]  # nova lista
+numericos = [1.5, 500, 30, 300, 80, 10]  # nova lista
 
 # Pegar o maior elemento da lista
 print(max(numericos))
@@ -30,6 +31,52 @@ print(max(numericos))
 # Pegar o menor elemento da lista
 print(min(numericos))
 
-# adicionar elemento na lista
-animais.append("Leao")
+# adicionar um elemento na lista ou outra lista
+animais.append("Leao")  # append adiciona um unico elemento na lista
 print(animais)
+animais.append(["Grilo", 14])  # append adiciona uma lista dentro de outra
+print(animais)
+
+# Adicionar mais de um elemento na lista
+animais.extend(["Cavalo", 10, "Carrapato"])
+print(animais)
+
+# Contando quantas vezes o elemento aprece na lista //count
+print(animais.count("Cavalo"))
+
+# Ordenar lista de numeros
+numericos.sort()
+print(numericos)
+
+# TUPLAS
+# As tuplas usam parenteses como sintaxe
+tuplazinha = ("Banana", "Maca", 10, 50)
+
+# Retornando o primeiro elemento
+print(tuplazinha[0])
+
+# Diferente das listas as tuplas sao imutaveis
+
+# count e slice funcionam com tuplas
+
+# DICIONARIOS
+
+# Criando dicionarios
+dicio = {"Maca": 20, "Banana": 10, "Laranja": 15, "Uva": 5}
+
+# Podemos acessar os valores dos dicionarios atraves da chave
+print(dicio["Maca"])
+
+# Podemos atualizar o valor de usando a chave
+dicio["Maca"] = 24
+
+# Retornamdo todas as chaves do dicionario
+print(dicio.keys())
+
+
+# Retornamdo todas os chaves do dicionario
+print(dicio.values())
+
+# Verificando se ja existe uma chave no dicionario e caso nao exista inserir
+dicio.setdefault("Limao", 22)
+print(dicio)
